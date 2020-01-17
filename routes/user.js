aeec5b0f0f2497077647ec4c78d0ca94ada2a95f
a86101102 <s0983196213@gmail.com>
2020-01-17 21:34:43 +0800

    signup finish

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../model/db');
 
 router.post('signup',function (req, res){
-    
+    var data = {
+        "studentId": req.body.studentId,
+        "department": req.body.department,
+        "grade": req.body.grade,
+        "email": req.body.email,
+        "name": req.body.name,
+        "password": req.body.password,
+        "position": req.body.position
+    }
+
+    console.log(data);
+
+    db.Insert("user", data, function(err, result){
+        if(err) console.log(err);
+    })
+    res.send("Create success!")
+
 })
 
 router.post('/login',function(req, res){
