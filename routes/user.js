5604385cd5f09431a504e6756f4a83da052d75dc
unknown <khjhs100266@gmail.com>
2020-01-17 21:59:11 +0800

    login api is completed

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../model/db');
 
 router.post('/signup',function (req, res){
     
 })
 
 router.post('/login',function(req, res){
+    var id = req.body.studentId;
+    var pw = req.body.password;
+    db.Query('SELECT password FROM `user` WHERE studentID='+ id ,function(password){
+        if(pw===password){
+            res.send({isLogin:"success"});
+        }
+        else{
+            res.send({isLogin:"fail"});
+        }
+    })
 
 })
 
+module.exports = router;
