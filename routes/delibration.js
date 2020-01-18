323c707c9c029d438e5cde3f469134790d1d47d0
meeeeeeeeow <ivy8792@gmail.com>
2020-01-18 21:25:00 +0800

    Add API entry the delibration

--- routes/delibration.js
+++ routes/delibration.js
@@ -1,3 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/delibration/entry',function(req, res){
+    var id = req.body.delibrationID;
+    var now = myDate.toLocaleString();
+    db.Query('SELECT semester,period,name,startTime,position FROM delibration WHERE delibrationID =' + id, function(result){
+        if(now >= result.startTime){
+             res.json(result);
+        }
+        else{
+           res.send("fail");
+        }
+    })
+})
