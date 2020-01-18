efd2e1ae3ae53220dc17009c72368691b7803862
unknown <khjhs100266@gmail.com>
2020-01-18 22:32:29 +0800

    api proposal/voteResults is done but not test yet

--- routes/proposal.js
+++ routes/proposal.js
@@ -12,4 +12,45 @@ router.get('/:delibrationID',function(req, res){
         }
     })
 })
+
+router.get('/voteResults',function(req, res){
+    var DID = req.body.delibrationID;
+    var PID = req.body.proposalID;
+    db.Query('SELECT vote FROM `vote` WHERE delibrationID='+ DId + ' and proposalID='+ PId , function(result, err){
+        if(err) {
+            console.log(err);
+        }
+        else{
+            var agree=0,disagree=0,total=0;
+            for (let n in result){
+                total=total+1;
+                if(result[n]==1){
+                    agree=agree+1;
+                }
+                if(result[n]==2){
+                    disagree=disagree+1;
+                }
+            }
+            var rate=agree/total;
+            var vote_result;
+            if(agree>disagree){
+                vote_result="同意";
+            }
+            else if(agree=disagree){
+                vote_result="重新投票";
+            }
+            else{
+                vote_result="反對";
+            }
+            var data={
+                "proposalID": DID,
+                "result": vote_result,
+                "vote": total,
+                "persent": rate
+            }
+        }
+        res.json(data);
+    })
+})
+
 module.exports = router;
