a11792e8f17094629736acea27da14d313598982
meeeeeeeeow <ivy8792@gmail.com>
2020-01-18 22:17:20 +0800

    Add API that change the isTime

--- routes/proposal.js
+++ routes/proposal.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/proposal/:delibrationID',function(req, res){
+router.get('/:delibrationID', function(req, res){
     var id = req.params.delibrationID;
     let col = ['提案單位'];
     db.FindbyColumn('proposal', col, {'delibrationID':id}, function(result, err){
@@ -9,6 +9,17 @@ router.get('/proposal/:delibrationID',function(req, res){
             res.send('No data');
         }else{
             res.json(result[0]);
+        }
+    })
+})
+
+router.get('/isVote', function(req, res){
+    var userId = req.body.studentID;
+    var delId = req.body.delibrationID;
+    var isTime = req.body.isTime;
+    db.Query('SELECT chair FROM delibration WHERE delibrationID =' + delId, function(chair){
+        if(userID === chair){
+            res.send("success");
         }
     })
 })
