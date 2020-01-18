e92ae914e5871e295d8c5c4179d71122a1a69899
meeeeeeeeow <ivy8792@gmail.com>
2020-01-18 22:33:59 +0800

    Update the query of entry API from name to dName

--- routes/delibration.js
+++ routes/delibration.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.get('/entry',function(req, res){
     var id = req.body.delibrationID;
     var now = myDate.toLocaleString();
-    db.Query('SELECT semester,period,name,startTime,position FROM delibration WHERE delibrationID =' + id, function(result){
+    db.Query('SELECT semester,period,dName,startTime,position FROM delibration WHERE delibrationID =' + id, function(result){
         if(now >= result.startTime){
              res.json(result);
         }
